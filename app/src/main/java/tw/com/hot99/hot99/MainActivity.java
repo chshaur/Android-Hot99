035218eedd6222a1f30bed92efd9b6c9e55169e7
chshaur <chshaur@gmail.com>
2017-06-09 12:00:02 +0800

    HOT99 2'nd Activity

--- app/src/main/java/tw/com/hot99/hot99/MainActivity.java
+++ app/src/main/java/tw/com/hot99/hot99/MainActivity.java
@@ -1,13 +1,45 @@
 package tw.com.hot99.hot99;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnEnter;
+    private Button btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("火紅超市")
+                        .setMessage("確定要離開?")
+                        .setPositiveButton("是", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNeutralButton("否", null)
+                        .show();
+
+
+            }
+        });
     }
+
+    private void findViews() {
+        btnEnter = (Button) findViewById(R.id.btn2web);
+        btnExit = (Button) findViewById(R.id.btnExit);
+    }
+
 }
---
+++ app/src/main/java/tw/com/hot99/hot99/WebActivity.java
@@ -0,0 +1,14 @@
+package tw.com.hot99.hot99;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+}
